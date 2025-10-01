a06cc479a0fd7f389855470a37b54bd7d9267df6
vono <vani4kamaksimov.2006@gmail.com>
2025-10-02 00:35:56 +0300

    no bags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Tech Blog')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50">
